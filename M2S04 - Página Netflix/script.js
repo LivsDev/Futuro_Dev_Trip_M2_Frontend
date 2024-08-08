8bdb9eae552a20434e17e31f594a0747a9cc47bd
LivsDev <livia_carvalho1@estudante.sesisenai.org.br>
2024-08-07 21:48:51 -0300

    método de repetição no JS para renderizar os cards no HTML (usar innerHTML)

--- M2S04 - Página Netflix/script.js
+++ M2S04 - Página Netflix/script.js
@@ -13,3 +13,24 @@ function episodioClicado(numero) {
 function episodioClicado(element) {
     alert(element.getAttribute('data-episodio') + ' clicado');
 }
+
+const episodios = [
+    'Episódio 1',
+    'Episódio 2',
+    'Episódio 3',
+    'Episódio 4',
+    'Episódio 5'
+];
+
+function renderizarEpisodios() {
+    const episodiosContainer = document.getElementById('episodios');
+    episodiosContainer.innerHTML = '';
+    
+    episodios.forEach((episodio, index) => {
+        episodiosContainer.innerHTML += `
+            <div class="card-episodio" data-episodio="${episodio}" onclick="episodioClicado(this)">${episodio}</div>
+        `;
+    });
+}
+
+renderizarEpisodios();

1a8a578291d86ef4393edd2e3050148d175c32dd
LivsDev <livia_carvalho1@estudante.sesisenai.org.br>
2024-08-08 18:08:03 -0300

    revisão e adicionado comentários no Código de JS

--- M2S04 - Página Netflix/script.js
+++ M2S04 - Página Netflix/script.js
@@ -14,21 +14,22 @@ function episodioClicado(element) {
     alert(element.getAttribute('data-episodio') + ' clicado');
 }
 
+// Array de episódios
 const episodios = [
     'Episódio 1',
     'Episódio 2',
     'Episódio 3',
 ];
-
+// Função para renderizar os episódios
 function renderizarEpisodios() {
     const episodiosContainer = document.getElementById('episodios');
-    episodiosContainer.innerHTML = '';
+    episodiosContainer.innerHTML = '';// Limpa o conteúdo existente
     
-    episodios.forEach((episodio, index) => {
+    episodios.forEach((episodio, index) => {  // Adiciona cada episódio ao contêiner usando `innerHTML`
         episodiosContainer.innerHTML += `
             <div class="card-episodio" data-episodio="${episodio}" onclick="episodioClicado(this)">${episodio}</div>
         `;
     });
 }
-
+// Chamar a função para renderizar os episódios
 renderizarEpisodios();
